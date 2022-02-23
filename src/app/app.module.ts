b3ca6774124dfab2cb2f2a18773fb6debb4e422a
cuong <ticuong436@gmail.com>
2022-02-23 17:01:07 +0700

    header and half homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomePageComponent } from './components/home-page/home-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
