d3dcec51d623488961b214564530c40a0423b493
nguyen ngoc cuong <ticuong436@gmail.com>
2022-03-02 08:24:58 +0700

    corner, answer, intro and new page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,41 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from "@angular/material/list"
+
 import { HomePagesComponent } from './components/home-pages/home-pages.component';
 
 import { SpecialzedComponent } from './components/specialzed/specialzed.component';
+import { NewEventComponent } from './components/new-event/new-event.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { LecturerPageComponent } from './sites/lecturer-page/lecturer-page.component';
+import { NewPageComponent } from './sites/new-page/new-page.component';
+import { ContactPageComponent } from './sites/contact-page/contact-page.component';
+import { IntroduceComponent } from './sites/introduce/introduce.component';
+import { HeaderComponent } from './components/header/header.component';
 
-
+import { CornerStudentComponent } from './sites/corner-student/corner-student.component';
+import { AnswerPageComponent } from './sites/answer-page/answer-page.component';
+import { ActivatePageComponent } from './sites/activate-page/activate-page.component';
+import { StudentFeelPageComponent } from './sites/student-feel-page/student-feel-page.component';
 
 
 
 @NgModule({
   declarations: [
+    
     AppComponent,
-    
-    
-          HomePagesComponent,
-                     
+          HomePagesComponent,   
                       SpecialzedComponent,
+                                             NewEventComponent,
+                                             IntroComponent,
+                                             LecturerPageComponent,
+                                             NewPageComponent,
+                                             ContactPageComponent,
+                                             IntroduceComponent,
+                                             HeaderComponent,
+                                         
+                                             CornerStudentComponent,
+                                                                                        AnswerPageComponent,
+                                                                                        ActivatePageComponent,
+                                                                                        StudentFeelPageComponent
                                        
                       
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    
+    AppRoutingModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
